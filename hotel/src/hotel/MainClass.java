e4730a9a8b6a76bd03be0f724435158e0eb96c8c
verson1 <user@DESKTOP-EKDGGEU>
2016-07-19 13:07:56 +1200

    verson1

---
+++ hotel/src/hotel/MainClass.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Room room1 = new Room("Suite", 120, 2, true);
+		room1.display();
+	}
+
+}
---
+++ hotel/src/hotel/Room.java
@@ -0,0 +1,34 @@
+package hotel;
+
+import java.lang.reflect.Array;
+
+public class Room {
+	private String name;
+	private double price;
+	int quantity;
+	boolean availability;
+	
+Array[][] room=new Array[5][5];
+
+public Room(String name, double price, int quantity, boolean availability) {
+	super();
+	this.name = name;
+	this.price = price;
+	this.quantity = quantity;
+	this.availability = availability;
+	
+}
+
+public void display(){
+	System.out.println("Hotel Name: "+name);
+	
+	
+	
+}
+
+	
+
+
+	
+}
+
